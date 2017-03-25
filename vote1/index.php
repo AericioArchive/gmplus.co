7bf7c65bd0d9ca7dbd39bff138d44c381d453aad
Aericio <xxechung16xx@gmail.com>
2017-03-24 15:21:51 -1000

    Voting and TOS

---
+++ vote1/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://minecraftpocket-servers.com/server/27727/vote/");
+exit;
+?>
---
+++ vote2/index.php
@@ -0,0 +1,4 @@
+<?php
+header("Location: https://minecraftpocket-servers.com/server/61182/vote/");
+exit;
+?>
